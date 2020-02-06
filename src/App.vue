f6720fd1145dc5eb70d0e1353b49888c74c03cc1
Elyor Latipov <elyor.blog@gmail.com>
2020-02-06 11:35:08 +0500

    first version use axios plugin

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <intro-content msg="Contacts" />
+    <infinite-scroll />
+  </div>
+</template>
+
+<script>
+import IntroContent from "./components/IntroContent.vue";
+import InfiniteScroll from "./components/InfiniteScroll.vue";
+
+export default {
+  name: "app",
+  components: {
+    IntroContent,
+    InfiniteScroll
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/IntroContent.vue
@@ -0,0 +1,21 @@
+
+<style scoped>
+  h1 {
+    margin: 40px 0 0;
+  } 
+</style>
+
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1> 
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'IntroContent',
+    props: {
+      msg: String
+    }
+  }
+</script>
